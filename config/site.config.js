8e98bad9f7c6e8b79773043f3762533f5d207f19
neomax8653 <161470581+neomax8653@users.noreply.github.com>
2024-03-19 15:32:39 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com" target="_blank" rel="noopener noreferrer">onedrive-index</a>. Made with ❤ by SaM.',
+    'Powered by <a href="https://github.com" target="_blank" rel="noopener noreferrer">Onedrive</a>. Made with ❤ by SaM.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
